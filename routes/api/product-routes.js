1ad4c1ddca6d8999b538dd319ea702883b6e310d
Daniel Bonn <danieltbonn@gmail.com>
2023-09-07 20:40:25 -0500

    added post route for tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -126,8 +126,27 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try { 
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({"message": "No products with this id." });
+      return;
+    }
+    console.log("Successfully deleted!")
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+    console.log("Error occured");
+    console.log(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,14 +49,54 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => {
+    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    if (req.body.productIds.length) {
+      const productTagIdArr = req.body.productIds.map((product_id) => {
+        return {
+          tag_id: tag.id,
+          product_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // if no product tags, we respond with
+    res.status(200).json(tag);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) => {
+    console.log("Error occured")
+    console.log(err);
+    res.status(400).json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try { 
+    const tagData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({"message": "No tags with this id." });
+      return;
+    }
+    console.log("Successfully deleted!")
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(500).json(err);
+    console.log("Error occured");
+    console.log(err);
+  }
 });
 
 module.exports = router;
