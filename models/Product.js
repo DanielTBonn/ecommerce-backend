943681f9c3272920d7efc20857f0dc4e8fa4f298
Daniel Bonn <danieltbonn@gmail.com>
2023-09-11 14:24:13 -0500

    added functionality to validate product price as a decimal and keep its precision to 2 decimal places where applicable, also added default value to stock being 10

--- models/Product.js
+++ models/Product.js
@@ -23,12 +23,19 @@ Product.init(
 
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
 
     },
     category_id: {
